80400b1d4b20dbd25bed31eeff6fc7789f339c1e
mengxianliang <mengxianliangok@163.com>
2017-05-15 11:28:31 +0800

    666

--- XLAVPlayerDemo/XLAVPlayerDemo/AppDelegate.m
+++ XLAVPlayerDemo/XLAVPlayerDemo/AppDelegate.m
@@ -17,9 +17,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    [self.window makeKeyAndVisible];
     self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ViewController new]];
     return YES;
 }
